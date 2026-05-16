3c211f055bdca24bc8d292a494dd23e75590655e
JoshYuJump <josh.yu_8@live.com>
2026-05-16 17:15:47 +0800

    Added serializer field MoneyField

--- nx/restframework/serializers.py
+++ nx/restframework/serializers.py
@@ -1,5 +1,16 @@
 from rest_framework import serializers
 
 
+class MoneyField(serializers.DecimalField):
+    """
+    A DecimalField pre-configured for monetary values with 18 digits and 2 decimal places.
+    """
+
+    def __init__(self, **kwargs):
+        kwargs.setdefault("max_digits", 18)
+        kwargs.setdefault("decimal_places", 2)
+        super().__init__(**kwargs)
+
+
 class MethodField(serializers.SerializerMethodField):
     pass
